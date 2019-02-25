46b3e5299af472e1b97d559a32a5542e1572327d
Samanth <samanthrao226@gmail.com>
2019-02-25 03:59:24 -0600

    s

--- Function.js
+++ Function.js
@@ -1,3 +1,8 @@
+$(document).ready(function(){
+    $("#myBtn").mouseleave(function () {
+           $("#test").hide();
+    });
+});
 function DollarConverter() {
     localStorage.setItem("num", parseFloat(document.getElementById("inputDollars").value));
     console.log(localStorage.getItem("num"));
@@ -9,7 +14,17 @@ localStorage.setItem("res",(localStorage.getItem("num")*70));
 console.log(localStorage.getItem("res"));
 document.getElementById("output").value=localStorage.getItem("res");
 }
-    
 }
 
-
+function mouse() {
+    $("#test").show();
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+           if (this.readyState == 4 && this.status == 200) {
+                  document.getElementById("test").innerHTML =
+                         this.responseText;
+           }
+    };
+    xhttp.open("GET", 'https://thesimpsonsquoteapi.glitch.me/quotes?count=4', true);
+    xhttp.send();
+}
--- JS/Qunit.js
+++ JS/Qunit.js
@@ -19,7 +19,7 @@ QUnit.test("Currencyconvertor", function(assert) {
     assert.equal(Currencyconvertor(50), 3500, "50 Dollars == 3500 INR. These are sucess conditions");
     assert.equal(Currencyconvertor(100), 7000, "100 Dollars == 7000 INR. These are sucess conditions");
     assert.equal(Currencyconvertor(500), 35000, "500 Dollars == 35000 INR. These are sucess conditions");
-    assert.equal(Currencyconvertor(-1), 0, "500 Dollars == 35000 INR. These are sucess conditions");
+    assert.equal(Currencyconvertor(-1), 35000, "500 Dollars == 35000 INR. These are sucess conditions");
 
 
  });
